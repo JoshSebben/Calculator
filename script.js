41d6d1aeb7054681f22e379ed0b2be436ff2672a
JoshSebben <sebbenjosh@gmail.com>
2025-01-06 12:46:44 -0500

    added operator functions for equations w/more than 3 nums

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function clearText(){
         userInput.removeChild(userInput.lastChild);
       }
 }
-
+// -- (For equations that deals with 2 numbers)
 function multiply(num1, num2){
     return num1 * num2
 }
@@ -36,4 +36,22 @@ function add(num1, num2){
 function subtract(num1, num2){
     return num1 - num2
 }
+// -- |
 
+// -- (Dealing with equations with more than 2 numbers)
+function sum(array){
+    return array.reduce((total, current) => total + current, 0)
+}
+
+function difference(array){
+    return array.reduce((total, current) => total - current)
+}
+
+function product(array){
+    return array.reduce((product, current) => product * current)
+}
+
+function quotient(array){
+    return array.reduce((quotient, current) => quotient / current)
+}
+// -- |
