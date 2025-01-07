7aab5a89adb2647f52007fc9d4342153d43846af
JoshSebben <sebbenjosh@gmail.com>
2025-01-07 12:53:51 -0500

    Added a DataStore for the numbers inputted by user

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const dataStore = []
+
 function changeText(text){
     const userInput = document.querySelector("#userRow")
     const newDiv = document.createElement("div")
 
-    //This is to fix the "divs with same ID" problem; 
+    //This is to fix the "divs with same ID" problem;
     const idNumber = Math.floor(Math.random() * (999999 - 1 + 1)) + 1;
     let textForm = idNumber.toString()
 
@@ -20,7 +22,7 @@ function clearText(){
         userInput.removeChild(userInput.lastChild);
       }
 }
-// -- (For equations that deals with 2 numbers)
+
 function multiply(num1, num2){
     return num1 * num2
 }
@@ -30,28 +32,9 @@ function divide(num1, num2){
 }
 
 function add(num1, num2){
-    return num1 + num2 //add later to accept strings
+    return num1 + num2 
 }
 
 function subtract(num1, num2){
     return num1 - num2
 }
-// -- |
-
-// -- (Dealing with equations with more than 2 numbers)
-function sum(array){
-    return array.reduce((total, current) => total + current, 0)
-}
-
-function difference(array){
-    return array.reduce((total, current) => total - current)
-}
-
-function product(array){
-    return array.reduce((product, current) => product * current)
-}
-
-function quotient(array){
-    return array.reduce((quotient, current) => quotient / current)
-}
-// -- |
