58544fc6a4cf9af12bf1fef3b89a70d325389e3d
JoshSebben <sebbenjosh@gmail.com>
2025-01-24 14:13:41 -0500

    Operate function now calculates and answer then replaces the text on the calculator

--- script.js
+++ script.js
@@ -46,22 +46,65 @@ function changeText(text){
 }
 
 
-function multiply(num1, num2){
+function multiply(num1, num2){ //Chooser == 1
     return num1 * num2
 }
 
-function divide(num1, num2){
+function divide(num1, num2){ //Chooser == 2
     return num1 / num2
 }
 
-function add(num1, num2){
+function add(num1, num2){ //Chooser == 3
     return num1 + num2 
 }
 
-function subtract(num1, num2){
+function subtract(num1, num2){ //Chooser == 4
     return num1 - num2
 }
 
 function operate(){
+    //Calculating answer and storing it into the dataStore
+
+    if (chooser == 1){
+        let product = multiply(dataStore[0], dataStore[1])
+        dataStore.pop()
+        dataStore.pop()
+        dataStore.push(product)
+    }
+    if (chooser == 2){
+        let quotient = divide(dataStore[0], dataStore[1])
+        dataStore.pop()
+        dataStore.pop()
+        dataStore.push(quotient)
+    }
+    if (chooser == 3){
+        let sum = add(dataStore[0], dataStore[1])
+        dataStore.pop()
+        dataStore.pop()
+        dataStore.push(sum)
+    }
+    if (chooser == 4){
+        let difference = subtract(dataStore[0], dataStore[1])
+        dataStore.pop()
+        dataStore.pop()
+        dataStore.push(difference)
+    }
+
+    //Replacing the text
+
+    clearText()
+
+    const userInput = document.querySelector("#userRow")
+    const newDiv = document.createElement("div")        
+    
+    const idNumber = Math.floor(Math.random() * (999999 - 1 + 1)) + 1;
+    let textForm = idNumber.toString()
+
+    newDiv.setAttribute("id", textForm)
+    userInput.appendChild(newDiv)
+
+    elem = document.getElementById(textForm);
+    elem.innerHTML = dataStore[0]; 
+
 
 }
